75a217f577033f4daf5783622582e424791c4a04
evamalis <evamalis@mail.ru>
2022-03-24 19:40:14 +0200

    module 14

---
+++ functions/n divide a.py
@@ -0,0 +1,6 @@
+def n_divide_a(n,a):
+    if n%a==0:
+        print("n divides to a")
+    else:
+        print("n does no divide to a")
+n_divide_a(10,5)
---
+++ functions/print 2 add 2.py
@@ -0,0 +1,5 @@
+def print_2_add_2():
+    sum=2+2
+    print(sum)
+
+print_2_add_2()
